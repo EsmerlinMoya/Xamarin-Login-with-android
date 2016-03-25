f1c09abec54d137dc6e2a0fc993f60e1134fc704
Esmerlin Rosario <merlin.moya@gmail.com>
2016-03-24 20:25:24 -0400

    Initial Commit

---
+++ LoginSystem/Dialog_SignUp.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace LoginSystem
+{
+	public class Dialog_SignUp : DialogFragment
+	{
+		public override Android.Views.View OnCreateView (Android.Views.LayoutInflater inflater, Android.Views.ViewGroup container, Android.OS.Bundle savedInstanceState)
+		{
+			base.OnCreateView (inflater, container, savedInstanceState);
+			var view = inflater.Inflate (Resource.Layout.dialog_sign_up, container, false);
+			return view;
+		}
+		public override void OnActivityCreated(Bundle savedInstanceState)
+		{
+			Dialog.Window.RequestFeature (WindowFeatures.NoTitle); //Set the Title Bar Invisible
+			base.OnActivityCreated (savedInstanceState);
+			Dialog.Window.Attributes.WindowAnimations = Resource.Style.dialog_animation; //Set the Animation
+		}
+
+	}
+}
+
---
+++ LoginSystem/MainActivity.cs
@@ -0,0 +1,30 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace LoginSystem
+{
+	[Activity (Label = "LoginSystem", MainLauncher = true, Icon = "@mipmap/icon")]
+	public class MainActivity : Activity
+	{
+
+		private Button mbtnsignup;
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			// Set our view from the "main" layout resource
+			SetContentView (Resource.Layout.Main);
+			mbtnsignup = FindViewById<Button> (Resource.Id.BtnSignUp);
+
+			mbtnsignup.Click += (object sender, System.EventArgs args) => {
+
+				//Pull Up Dialog
+				FragmentTransaction transaction = FragmentManager.BeginTransaction();
+				Dialog_SignUp signup = new Dialog_SignUp();
+				signup.Show(transaction,"Dialog Fragment");
+			};
+
+		}
+	}
+}
